eb054d011886c438c6cf0144508e9f72ca1929fc
Germain Boue <boue.germain@gmail.com>
2017-02-16 21:22:17 -0800

    Define RootQuery, still need to add hardcoded data to test

--- schema/schema.js
+++ schema/schema.js
@@ -16,3 +16,19 @@ const UserType = new GraphQLObjectType({
     age: { type: GraphQLInt }
   }
 });
+
+// root query lets us jump in our graph of data. here we are saying if you give me id of User 
+// you are looking for, I will return that User to you
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString} },
+      resolve(parentValue, args) {
+        //we go in our DB and actually find that data.
+      }
+    }
+  }
+})
