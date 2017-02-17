77fbe2781f27c670172b93a5bb0fc6428d898492
Germain Boue <boue.germain@gmail.com>
2017-02-16 20:43:13 -0800

    Check that we are linked to expressGraphQL

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 // where all the logic living on the server side of our app lives
 
 const express = require('express');
+const expressGraphQL = require('express-graphql');
 
 const app = express();
+
+// we want to let our app know that everytime we hit the url /graphQL to use expressGraphQL
+// graphiql is used to query our development env server
+app.use('/graphQL', expressGraphQL({
+  graphiql: true
+}))
 
 app.listen(4000, () => {
   console.log('Listening');
